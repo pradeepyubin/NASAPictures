3ad800e674736cf0d8fc9b0afca6dd4e5aaeeae8
pradeep-5809 <pradeep.k@zohocorp.com>
2022-05-30 22:55:14 +0530

    Adding default loading image for Glide & Changing from Toast to Snackbar for Network error

--- app/src/main/java/com/nasa/myapplication/ui/adapter/BindingAdapters.kt
+++ app/src/main/java/com/nasa/myapplication/ui/adapter/BindingAdapters.kt
@@ -3,9 +3,11 @@ package com.nasa.myapplication.ui.adapter
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.nasa.myapplication.R
 
 @BindingAdapter("picture_url")
 fun pictureUrl(imageView: ImageView, url: String) =
     Glide.with(imageView)
         .load(url)
+        .placeholder(R.drawable.default_image)
         .into(imageView)
--- app/src/main/java/com/nasa/myapplication/ui/view/activity/MainActivity.kt
+++ app/src/main/java/com/nasa/myapplication/ui/view/activity/MainActivity.kt
@@ -3,7 +3,9 @@ package com.nasa.myapplication.ui.view.activity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import com.nasa.myapplication.R
+import com.nasa.myapplication.databinding.ActivityMainBinding
 import com.nasa.myapplication.util.ConnectivityManager
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -12,17 +14,19 @@ import javax.inject.Inject
 class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var connectivityManager: ConnectivityManager
+    lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         initObserver()
     }
 
     private fun initObserver() {
         connectivityManager.isNetworkAvailable.observe(this) { isAvailable ->
            if(!isAvailable) {
-               Toast.makeText(this,"Please connect to network",Toast.LENGTH_SHORT).show()
+               Snackbar.make(binding.root, "Please connect to network", Snackbar.LENGTH_LONG).show()
            }
         }
     }
